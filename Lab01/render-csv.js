ada07492f79de2666c25b466104373651dd8870b
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-09-02 19:41:06 -0400

    Cleaning up code

--- Lab01/render-csv.js
+++ Lab01/render-csv.js
@@ -9,7 +9,10 @@ function set_data(lines) {
 }
 
 function csv_draw_bars(species) {
+  // Reset averages
   avgs = [0, 0, 0, 0];
+
+  // Sum data
   for (var row = 1; row < data.length; row++) {
     for (var col = 1; col < data[row].length; col++) {
       if (data[row][0] === species) {
@@ -17,14 +20,13 @@ function csv_draw_bars(species) {
       }
     }
   }
-  console.log(avgs);
+
+  // Average data
   for (var j = 0; j < data[0].length; j++) {
     avgs[j] = avgs[j] / (data.length - 1);
-    console.log(" column " + j + " Avg = " + avgs[j]);
   }
 
+  // Signal new data
   has_avgs = true;
-
   createBarVertices(avgs);
-
 }
