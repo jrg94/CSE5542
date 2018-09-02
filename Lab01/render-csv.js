85573c150167ae93bedb5e3885027169c0a6b7bb
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-09-02 19:38:36 -0400

    Rendered setosa averages

--- Lab01/render-csv.js
+++ Lab01/render-csv.js
@@ -8,23 +8,19 @@ function set_data(lines) {
   has_data = true;
 }
 
-function csv_draw_bars() {
-
-  for (var i = 0; i < data.length; i++) {
-    for (var j = 0; j < data[i].length; j++) {
-      if (i == 0) {
-        tmp = 0;
-        avgs.push(tmp);
-      } else {
-        if (j == 0) avgs[j] = 0;
-        else avgs[j] += Number(data[i][j]);
+function csv_draw_bars(species) {
+  avgs = [0, 0, 0, 0];
+  for (var row = 1; row < data.length; row++) {
+    for (var col = 1; col < data[row].length; col++) {
+      if (data[row][0] === species) {
+        avgs[col - 1] += Number(data[row][col]);
       }
     }
   }
+  console.log(avgs);
   for (var j = 0; j < data[0].length; j++) {
     avgs[j] = avgs[j] / (data.length - 1);
     console.log(" column " + j + " Avg = " + avgs[j]);
-
   }
 
   has_avgs = true;
