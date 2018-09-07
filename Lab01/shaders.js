488bedcca1129e104ecb6977f8b74ae3b3613339
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-09-07 18:42:44 -0400

    Updated shaders

--- Lab01/shaders.js
+++ Lab01/shaders.js
@@ -1,15 +1,19 @@
 var vertexShaderSrc = `
   attribute vec3 aVertexPosition;
+  attribute vec4 aVertexColor;
+  varying vec4 vColor;
 
   void main(void) {
     gl_Position = vec4(aVertexPosition, 1.0);
+    vColor = aVertexColor;
   }
 `;
 
 var fragmentShaderSrc = `
   precision mediump float;
+  verying vec4 vColor;
 
   void main(void) {
-    gl_FragColor = vec4(1,0,0,1);
+    gl_FragColor = vColor;
   }
 `;
