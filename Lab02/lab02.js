5b704800cba9928a7b05ddee74459f01242366ad
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-09-28 15:46:35 -0400

    Moved all reset operations to hierarchy

--- Lab02/lab02.js
+++ Lab02/lab02.js
@@ -55,10 +55,13 @@ var root = generateHierarchy();
  * @param {!Array<number>} axes a set of axes vertices for this node
  * @param {!Array<!Node>} children a set of children nodes
  */
-function Node(id, vertices, axes, children) {
+function Node(id, vertices, axes, initTranslation, initRotation, initScale, children) {
   this.id = id;
   this.vertices = vertices;
   this.axes = axes;
+  this.initTranslation = initTranslation;
+  this.initRotation = initRotation;
+  this.initScale = initScale;
   this.mvMatrix = null;
   this.children = children;
 
@@ -95,6 +98,24 @@ function Node(id, vertices, axes, children) {
     return item;
   }
 
+  // Implements the reset feature
+  this.reset = function() {
+    this.initMVMatrix();
+    console.log("Resetting: " + this.id);
+    if (this.initTranslation !== null) {
+      this.translate(this.initTranslation);
+    }
+    if (this.initRotation !== null) {
+      this.rotate(this.initRotation);
+    }
+    if (this.initScale !== null) {
+      this.scale(this.initScale);
+    }
+    children.forEach(function(node) {
+      node.reset();
+    });
+  }
+
   this.initMVMatrix = function() {
     var mvMatrix = mat4.create();
     this.mvMatrix = mat4.identity(mvMatrix);
@@ -129,25 +150,25 @@ function Node(id, vertices, axes, children) {
  * Generates an object hierarchy.
  */
 function generateHierarchy() {
-  var root = new Node("body", SQUARE, AXES, [
-    new Node("head", SQUARE, AXES, []),
-    new Node("top-left-femur", SQUARE, AXES, [
-      new Node("top-left-tibia", SQUARE, AXES, [])
+  var root = new Node("body", SQUARE, AXES, null, null, [0.25, 0.25, 0.25], [
+    new Node("head", SQUARE, AXES, [.75, 0, 0], null, [.5, .5, .5], []),
+    new Node("top-left-femur", SQUARE, AXES, [0.35, .75, 0], degToRad(-45.0), [.20, .50, .35], [
+      new Node("top-left-tibia", SQUARE, AXES, [0.0, 1.0, 0], degToRad(90), null, [])
     ]),
-    new Node("middle-left-femur", SQUARE, AXES, [
-      new Node("middle-left-tibia", SQUARE, AXES, [])
+    new Node("middle-left-femur", SQUARE, AXES, [0.0, .75, 0], degToRad(-45.0), [.20, .50, .35], [
+      new Node("middle-left-tibia", SQUARE, AXES, [0.0, 1.0, 0], degToRad(90), null, [])
     ]),
-    new Node("bottom-left-femur", SQUARE, AXES, [
-      new Node("bottom-left-tibia", SQUARE, AXES, [])
+    new Node("bottom-left-femur", SQUARE, AXES, [-0.35, .75, 0], degToRad(-45.0), [.20, .50, .35], [
+      new Node("bottom-left-tibia", SQUARE, AXES, [0.0, 1.0, 0], degToRad(90), null, [])
     ]),
-    new Node("top-right-femur", SQUARE, AXES, [
-      new Node("top-right-tibia", SQUARE, AXES, [])
+    new Node("top-right-femur", SQUARE, AXES, [0.35, -.75, 0], degToRad(45.0), [.20, .50, .35], [
+      new Node("top-right-tibia", SQUARE, AXES, [0.0, -1.0, 0], degToRad(-90), null, [])
     ]),
-    new Node("middle-right-femur", SQUARE, AXES, [
-      new Node("middle-right-tibia", SQUARE, AXES, [])
+    new Node("middle-right-femur", SQUARE, AXES, [0.00, -.75, 0], degToRad(45.0), [.20, .50, .35], [
+      new Node("middle-right-tibia", SQUARE, AXES, [0.0, -1.0, 0], degToRad(-90), null, [])
     ]),
-    new Node("bottom-right-femur", SQUARE, AXES, [
-      new Node("bottom-right-tibia", SQUARE, AXES, [])
+    new Node("bottom-right-femur", SQUARE, AXES, [-0.35, -.75, 0], degToRad(45.0), [.20, .50, .35], [
+      new Node("bottom-right-tibia", SQUARE, AXES, [0.0, -1.0, 0], degToRad(-90), null, [])
     ])
   ])
   console.log(root);
@@ -405,53 +426,8 @@ function webGLStart() {
   document.addEventListener('mousedown', onDocumentMouseDown, false);
   document.addEventListener('keydown', onKeyDown, false);
 
-  transformHierarchy();
+  root.reset();
   drawScene();
-}
-
-/**
- * Initializes the hierarchy by transforming all nodes.
- */
-function transformHierarchy() {
-  // Top level
-  initNode("body", null, null, [0.25, 0.25, 0.25]);
-
-  initNode("head", [.75, 0, 0], null, [.5, .5, .5]);
-
-  initNode("top-left-femur", [0.35, .75, 0], degToRad(-45.0), [.20, .50, .35]);
-  initNode("top-left-tibia", [0.0, 1.0, 0], null, null);
-
-  initNode("middle-left-femur", [0.0, .75, 0], null, [.20, .50, .35]);
-  initNode("middle-left-tibia", [0.0, 1.0, 0], null, null);
-
-  initNode("bottom-left-femur", [-0.35, .75, 0], null, [.20, .50, .35]);
-  initNode("bottom-left-tibia", [0.0, 1.0, 0], null, null);
-
-  initNode("top-right-femur", [0.35, -.75, 0], null, [.20, .50, .35]);
-  initNode("top-right-tibia", [0.0, -1.0, 0], null, null);
-
-  initNode("middle-right-femur", [0.00, -.75, 0], null, [.20, .50, .35]);
-  initNode("middle-right-tibia", [0.0, -1.0, 0], null, null);
-
-  initNode("bottom-right-femur", [-0.35, -.75, 0], null, [.20, .50, .35]);
-  initNode("bottom-right-tibia", [0.0, -1.0, 0], null, null);
-}
-
-/**
- * Sets some initial conditions for a node.
- */
-function initNode(id, translate, rotate, scale) {
-  var node = root.search(id);
-  node.initMVMatrix();
-  if (translate !== null) {
-    node.translate(translate);
-  }
-  if (rotate !== null) {
-    node.rotate(rotate);
-  }
-  if (scale !== null) {
-    node.scale(scale);
-  }
 }
 
 /**
@@ -470,7 +446,7 @@ function backgroundColor(red, green, blue) {
  * Resets the scene to it's original appearance.
  */
 function redraw() {
-  transformHierarchy();
+  root.reset();
   drawScene();
 }
 
