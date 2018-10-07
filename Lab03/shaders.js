5994a129ce5465e60319915e0d7010b3ac63176e
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-10-07 17:57:01 -0400

    Moved fragment shader from HTML to JS

--- Lab03/shaders.js
+++ Lab03/shaders.js
@@ -19,6 +19,24 @@ var vertexShaderSrc = `
 // The fragment shader
 var fragmentShaderSrc = `
   precision mediump float;
+
+  uniform mat4 uMMatrix;
+  uniform mat4 uVMatrix;
+  uniform mat4 uPMatrix;
+  uniform mat4 uNMatrix;
+
+  uniform vec4 light_pos;
+  uniform vec4 ambient_coef;
+  uniform vec4 diffuse_coef;
+  uniform vec4 specular_coef;
+  uniform float mat_shininess;
+
+  uniform vec4 light_ambient;
+  uniform vec4 light_diffuse;
+  uniform vec4 light_specular;
+
+  varying vec4 eye_pos;
+  varying vec3 v_normal;
   varying vec4 vColor;
 
   void main(void) {
