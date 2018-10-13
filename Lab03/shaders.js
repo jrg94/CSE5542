780e8ed61d736e60a4295e17bb1f79fb0fa6e7fd
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-10-12 20:37:54 -0400

    Removed all color calculations

--- Lab03/shaders.js
+++ Lab03/shaders.js
@@ -20,49 +20,12 @@ var vertexShaderSrc = `
   uniform vec4 light_diffuse;
   uniform vec4 light_specular;
 
-  varying vec4 eye_pos;  //vertex position in eye space
-  varying vec3 v_normal;  // vertex normal
-  varying vec4 vColor;
+  varying vec4 eye_pos;
+  varying vec3 v_normal;
 
   void main(void) {
-
-    // transform light pos from local to eye space
-    vec4 light_pos_in_eye = light_pos;
-
-    // transform normal from local to eye space: normal matrix is the inverse transpose of the modelview matrix
-    v_normal =normalize(vec3(uNMatrix*vec4(aVertexNormal,1.0)));
-
-    // transform the vertex position to eye space
-    eye_pos = uVMatrix*uMMatrix*vec4(aVertexPosition, 1.0);
-
-    // light vector L = l-p
-    vec3 light_vector = normalize(vec3(light_pos_in_eye - eye_pos));
-
-    // eye vector V = e-p, where e is (0,0,0)
-    vec3 eye_vector = normalize(-vec3(eye_pos));
-
-    // halfway vector (L+V)
-    vec3 halfv = normalize(light_vector+eye_vector);
-
-    vec4 ambient = ambient_coef * light_ambient;
-    float ndotl = max(dot(v_normal, light_vector), 0.0);
-
-    vec4 diffuse = diffuse_coef * light_diffuse * ndotl;
-
-    vec3 R = normalize(2.0 * ndotl * v_normal - eye_vector);
-    float rdotv = max(dot(R, eye_vector), 0.0);
-
-    vec4 specular;
-    if (ndotl > 0.0) {
-      specular = specular_coef * light_specular * pow(rdotv, mat_shininess);
-    } else {
-      specular = vec4(0, 0, 0, 1);
-    }
-
     gl_Position = uPMatrix * uVMatrix * uMMatrix * vec4(aVertexPosition, 1.0);
-
-    vColor = ambient + diffuse + specular;
-    }
+  }
 `;
 
 // The fragment shader
