1482cf8bd42746f9611777ca2e4131f12f2e3b36
Jeremy Grifski <jeremy.grifski@gmail.com>
2018-11-06 10:40:12 -0500

    Moved vertex shader

--- Lab04/JS/shaders.js
+++ Lab04/JS/shaders.js
@@ -1,13 +1,18 @@
 // The vertex shader
 var vertexShaderSrc = `
   precision mediump float;
+  precision mediump int;
+
   attribute vec3 aVertexPosition;
   attribute vec3 aVertexNormal;
+  attribute vec2 aVertexTexCoords;
+  attribute vec3 aVertexColor;
 
   uniform mat4 uMMatrix;
   uniform mat4 uVMatrix;
   uniform mat4 uPMatrix;
   uniform mat4 uNMatrix;
+  uniform mat4 uV2WMatrix;  // eye to world
 
   uniform vec4 light_pos;
   uniform vec4 ambient_coef;
@@ -19,15 +24,64 @@ var vertexShaderSrc = `
   uniform vec4 light_diffuse;
   uniform vec4 light_specular;
 
-  varying vec3 v_normal;
-  varying vec3 v_pos;
+  uniform int use_texture;
+  uniform sampler2D myTexture;
+
+  varying vec4 eye_pos;  //vertex position in eye space
+  varying vec3 v_normal;  // vertex normal
+  varying highp vec2 FtexCoord;
+  varying vec4 vColor;
+
 
   void main(void) {
-    gl_Position = uPMatrix * uVMatrix * uMMatrix * vec4(aVertexPosition, 1.0);
-    vec4 v_pos4 = uVMatrix * uMMatrix * vec4(aVertexPosition, 1.0);
-    v_pos = vec3(v_pos4) / v_pos4.w;
-    v_normal = vec3(uNMatrix * vec4(aVertexNormal, 0.0));
-  }
+
+    // transform light pos from local to eye space
+    // vec4 light_pos_in_eye = uVMatrix * uMMatrix * light_pos;
+    vec4 light_pos_in_eye = light_pos;
+
+    // transform normal from local to eye space: normal matrix is the inverse transpose of the modelview matrix
+    v_normal =normalize(vec3(uNMatrix*vec4(aVertexNormal,0.0)));
+
+    // transform the vertex position to eye space
+    eye_pos = uVMatrix*uMMatrix*vec4(aVertexPosition, 1.0);
+
+    // light vector L = l-p
+    vec3 light_vector = normalize(vec3(light_pos_in_eye - eye_pos));
+
+    // eye vector V = e-p, where e is (0,0,0)
+    vec3 eye_vector = normalize(-vec3(eye_pos));
+
+    vec4 ambient = ambient_coef * light_ambient;
+    float ndotl = max(dot(v_normal, light_vector), 0.0);
+
+    vec4 diffuse = diffuse_coef * light_diffuse* ndotl;
+
+    //      both lines below are okay. One is to use the reflect function the other is to compute by yourself
+    //      vec3 R= normalize(vec3(reflect(-light_vector, v_normal)));
+    vec3 R = normalize(2.0 * ndotl *v_normal-light_vector);
+    float rdotv = max(dot(R, eye_vector), 0.0);
+
+
+    vec4 specular;
+    if (ndotl > 0.0) {
+      specular = specular_coef* light_specular*pow(rdotv, mat_shininess);
+    } else {
+      specular = vec4(0,0,0,1);
+    }
+
+    vColor = ambient+diffuse+specular;
+
+    FtexCoord = aVertexTexCoords;
+
+    gl_Position = uPMatrix*uVMatrix*uMMatrix*vec4(aVertexPosition, 1.0);
+
+    //      vColor = vec4(use_texture, use_texture, 0, 1.0);
+    //      vColor = vec4(aVertexTexCoords, 1.0, 1.0);
+    //      vColor = vec4(aVertexColor,1.0);
+    //      vColor = vec4(aVertexNormal, 1.0);
+    //      vColor = vec4(v_normal, 1.0);
+
+}
 `;
 
 // The fragment shader
