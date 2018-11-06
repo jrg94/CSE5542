030b44ab26f6bdb12c642a56dd966b085b2cb5d9
Jeremy Grifski <jeremy.grifski@gmail.com>
2018-11-06 10:32:25 -0500

    Moved files from lab03

---
+++ Lab04/JS/shader_setup.js
@@ -0,0 +1,24 @@
+/**
+ * Initializes the shaders given in shaders.js.
+ */
+function initShaders() {
+  shaderProgram = gl.createProgram();
+
+  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShader, vertexShaderSrc);
+  gl.compileShader(vertexShader);
+
+  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShader, fragmentShaderSrc);
+  gl.compileShader(fragmentShader);
+
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert("Could not initialise shaders");
+  }
+
+  gl.useProgram(shaderProgram);
+}
