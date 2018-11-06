56bfe2eef566589396417c0a6c33d3a0e7ee5578
Jeremy Grifski <jeremy.grifski@gmail.com>
2018-11-06 10:37:05 -0500

    Moved fragment shader

--- Lab04/JS/shaders.js
+++ Lab04/JS/shaders.js
@@ -33,11 +33,13 @@ var vertexShaderSrc = `
 // The fragment shader
 var fragmentShaderSrc = `
   precision mediump float;
+  precision mediump int;
 
-  uniform mat4 uMMatrix;
-  uniform mat4 uVMatrix;
-  uniform mat4 uPMatrix;
-  uniform mat4 uNMatrix;
+  uniform mat4 uMMatrix;    // model matrix
+  uniform mat4 uVMatrix;    // view matrix
+  uniform mat4 uPMatrix;    // projection matrix
+  uniform mat4 uNMatrix;    // normal matrix
+  uniform mat4 uV2WMatrix;  // eye to world
 
   uniform vec4 light_pos;
   uniform vec4 ambient_coef;
@@ -49,27 +51,34 @@ var fragmentShaderSrc = `
   uniform vec4 light_diffuse;
   uniform vec4 light_specular;
 
+  uniform int use_texture;
+  uniform sampler2D myTexture;
+  uniform samplerCube cubeMap;
+
+  varying vec4 eye_pos;
   varying vec3 v_normal;
-  varying vec3 v_pos;
+  varying highp vec2 FtexCoord;
+  varying vec4 vColor;
 
   void main(void) {
-    vec3 normal = normalize(v_normal);
-    vec3 lightDir = normalize(vec3(light_pos) - v_pos);
-    vec3 reflectDir = reflect(-lightDir, v_normal);
-    vec3 viewDir = normalize(-v_pos);
 
-    float lambertian = max(dot(lightDir, normal), 0.0);
-    float spec = 0.0;
+    vec4 texcolor;
+    vec3 view_vector, ref;
+    vec4 env_color = vec4(1,0,0,1);
 
-    if (lambertian > 0.0) {
-       float specAngle = max(dot(reflectDir, viewDir), 0.0);
-       spec = pow(specAngle, mat_shininess);
-    }
-
-    vec4 ambient = ambient_coef * light_ambient;
-    vec4 diffuse = diffuse_coef * light_diffuse;
-    vec4 specular = specular_coef * light_specular;
-    vec4 color = ambient + lambertian * diffuse + spec * specular;
-    gl_FragColor = color;
-  }
+    if ( use_texture == 1 ) {
+      texcolor = texture2D(myTexture, FtexCoord);
+      gl_FragColor = texcolor;
+      // gl_FragColor = vColor*texcolor;
+    } else if (use_texture == 2) {
+       view_vector = normalize(vec3(vec4(0,0,0,1)-eye_pos));
+       ref = normalize(reflect(-view_vector, v_normal));  // in eye space
+       ref = vec3(uV2WMatrix*vec4(ref,0));   // convert to world space
+       env_color = textureCube(cubeMap, ref);
+       gl_FragColor = env_color;
+       // gl_FragColor = vec4(ref, 1.0);
+     } else {
+       gl_FragColor = vColor;
+     }
+}
 `;
